651538a6b5efba8589a3a227f9d84e86b479efa3
Gaël Reyrol <me@gaelreyrol.com>
2019-10-06 11:32:22 +0200

    chore: update vue-segment

--- test/fixture/pages/index.vue
+++ test/fixture/pages/index.vue
@@ -1,24 +1,26 @@
 <template>
   <div>
     Works!
-    <button @click="loadSegment">Load Segment</button>
+    <button @click="loadSegment">
+      Load Segment
+    </button>
   </div>
 </template>
 
 <script>
-  export default {
-    mounted () {
-      this.$segment.identify('f4ca124298', {
-        name: 'Michael Bolton',
-        email: 'mbolton@initech.com'
-      })
-      this.$segment.track('Signed Up', { plan: 'Enterprise' })
-      this.$segment.page()
-    },
-    methods: {
-      loadSegment() {
-        this.$segment.load(process.env.SEGMENT_WRITE_KEY)
-      }
+export default {
+  mounted () {
+    this.$segment.identify('f4ca124298', {
+      name: 'Michael Bolton',
+      email: 'mbolton@initech.com'
+    })
+    this.$segment.track('Signed Up', { plan: 'Enterprise' })
+    this.$segment.page()
+  },
+  methods: {
+    loadSegment () {
+      this.$segment.load(process.env.SEGMENT_WRITE_KEY)
     }
   }
+}
 </script>
